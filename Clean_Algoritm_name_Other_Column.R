f1b378db9270799e323fbe709fb10509c1a57bbe
Leacavalli <leajm.cavalli@gmail.com>
2024-06-16 17:55:54 -0400

    Update Data cleaning

--- Clean_Algoritm_name_Other_Column.R
+++ Clean_Algoritm_name_Other_Column.R
@@ -4,22 +4,21 @@ rm( list = ls() )
 # load necessary libraries
 library(tidyverse)
 
-# Load data
+# Load covidence raw data
 setwd("~/GitHub/Calibration-Review")
-raw_data <- read.csv("review_396964_20240608035120.csv")
+Data_Calibration_Algorithm <- read.csv("Data_Calibration_Algorithm.csv")
 
-
-# Recode 
+# Make vectors containing all versions of each algorithm's name
 ABC_SMC_names <- c("Approximate Bayesian Computation Sequential Monte Carlo scheme", 
                    "Approximate Bayesian computation with sequential Monte Carlo sampling", 
                    "Approximate Bayesian Computation with Sequential Monte Carlo Sampling", 
-                   "Aproximate Bayesian Computation Sequential Monte Carlo (ABC-SMC) algorithm ")
-
+                   "Aproximate Bayesian Computation Sequential Monte Carlo (ABC-SMC) algorithm ", 
+                   "Approximate Bayesian Computation Sequential Monte Carlo (ABC SMC) method", 
+                   "Approximate Bayesian Computation with Sequential Monte Carlo sampling (ABC-SMC) method")
 HME_names <- c("History matching with emulation", 
                "history matching with model emulation", 
                "history matching with emulation", 
                " history matching with emulation")
-
 IMIS_names <- c("incremental mixture importance sampling", 
                 "Incremental mixture importance sampling", 
                 "Incremental Mixture Importance Sampling", 
@@ -28,62 +27,37 @@ IMIS_names <- c("incremental mixture importance sampling",
                 "Incremental Mixture Importance Sampling (IMIS) ",
                 "a Bayesian framework with incremental mixture importance sampling", 
                 "Incremental Mixture Importance Sampling")
-
 MCF_names <- c("Monte-Carlo filtering", 
                "Monte-Carlo filtering ", 
                "Monte Carlo filtering")
-
-
 BM_names <- c("Bayesian melding",
               " Bayesian melding")
-
 NM_names <- c("Iterative, directed-search Nelder-Mead (NM) method", 
-              "Nelder-Mead algorithm ")
-
+              "Nelder-Mead algorithm ", 
+              "Nelder-Mead")
 SIR_names <- c("sample-importance-resampling ", 
                "Sampling importance resampling ",
                "Sampling Importance Resampling",
-               "Semi-Bayesian Sampling/Importance-Resampling algorithm")
-
+               "Semi-Bayesian Sampling/Importance-Resampling algorithm", 
+               "Sampling-importance-resampling (SIR) approach")
 SMC_PF_names <- c("sequential Monte Carlo method based on particle filtering", 
                   "sequential Monte Carlo method based on particle filtering",
-                  "Sampling Importance Resampling",
+                  "particle Markov\nchain Monte Carlo (PMCMC",
                   "Sequential Monte Carlo method based on particle filtering known as\nMIF for Likelihood Maximization by Iterated Filtering")
-
 PSPO_names <- c("parallel simultaneous perturbation \noptimisation (PSPO)",
                 "parallel simultaneous perturbation optimization ")
 
+# Recode the Other algorithm names with standardized name 
+Data_Calibration_Algorithm_v2 <- Data_Calibration_Algorithm |>
+  mutate(Other_Name.of.calibration.algorithm_V2= ifelse(Other_Name.of.calibration.algorithm %in% ABC_SMC_names, "Aproximate Bayesian Computation Sequential Monte Carlo (ABC-SMC)", 
+                                                        ifelse(Other_Name.of.calibration.algorithm %in% HME_names, "History matching with emulation",
+                                                               ifelse(Other_Name.of.calibration.algorithm %in% IMIS_names, "Incremental Mixture Importance Sampling (IMIS)", 
+                                                                      ifelse(Other_Name.of.calibration.algorithm %in% MCF_names, "Monte-Carlo filtering", 
+                                                                             ifelse(Other_Name.of.calibration.algorithm %in% SIR_names, "Sampling importance resampling", 
+                                                                                    ifelse(Other_Name.of.calibration.algorithm %in% SMC_PF_names, "Sequential Monte Carlo method based on particle filtering", 
+                                                                                           ifelse(Other_Name.of.calibration.algorithm %in% BM_names, "Bayesian melding", 
+                                                                                                  ifelse(Other_Name.of.calibration.algorithm %in% NM_names, "Nelder-Mead algorithm", 
+                                                                                                         ifelse(Other_Name.of.calibration.algorithm %in% PSPO_names, "Parallel Simultaneous perturbation Optimisation (PSPO)", Other_Name.of.calibration.algorithm))))))))))
 
-raw_data_Calibration_Algorithm_other <- raw_data |>
-  mutate(Other_V2= ifelse(X.Other..details..2 %in% ABC_SMC_names, "Aproximate Bayesian Computation Sequential Monte Carlo (ABC-SMC)", 
-                          ifelse(X.Other..details..2 %in% HME_names, "History matching with emulation",
-                                 ifelse(X.Other..details..2 %in% IMIS_names, "Incremental Mixture Importance Sampling (IMIS)", 
-                                        ifelse(X.Other..details..2 %in% MCF_names, "Monte-Carlo filtering", 
-                                               ifelse(X.Other..details..2 %in% SIR_names, "Sampling importance resampling", 
-                                                      ifelse(X.Other..details..2 %in% SMC_PF_names, "Sequential Monte Carlo method based on particle filtering", 
-                                                             ifelse(X.Other..details..2 %in% BM_names, "Bayesian melding", 
-                                                                    ifelse(X.Other..details..2 %in% NM_names, "Nelder-Mead algorithm", 
-                                                                           ifelse(X.Other..details..2 %in% PSPO_names, "Parallel Simultaneous perturbation Optimisation (PSPO)", X.Other..details..2)))))))))) |>
-  rename("Other" = "X.Other..details..2")
-
-
-
-raw_data_Calibration_Algorithm_other |>
-  filter(Name.of.calibration.algorithm == "Other ") |>
-  group_by(Other_V2) |>
-  summarise(N=n())  |>
-  filter(N>1)|>
-  arrange(-N)
-
-#
-raw_data_Calibration_Algorithm_other |>
-  filter(Name.of.calibration.algorithm != "Other ") |>
-  filter(Other !="")|>
-  select(Covidence.., Study.ID, Name.of.calibration.algorithm, Other)
-
-raw_data_Calibration_Algorithm_other_subset <- raw_data_Calibration_Algorithm_other |>
-  select(Covidence.., Study.ID, Name.of.calibration.algorithm, Other, Other_V2)
-
-
-
-write.csv(raw_data_Calibration_Algorithm_other_subset, "Data_Calibration_Algorithm.csv", row.names = F)
+# Write the new file 
+# write.csv(Data_Calibration_Algorithm_v2, "Data_Calibration_Algorithm.csv", row.names = F)
